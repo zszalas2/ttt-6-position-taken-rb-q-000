b6a037670a1e4019fd4b6c030686154bf7f1fd8e
zszalas2 <zszalas@gmail.com>
2015-12-13 07:50:56 +0100

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,18 @@
 # code your #position_taken? method here!
+# 1. Define a method that updates an array passed into it as an argument.
+# 2. Define a method that uses control flow in the form of `if/else` statements to manipulate an array.
+# expectations
+#we expect the player to input a move
+#we expect the computer to accept the move or reject the move based on whether the position is taken (position_taken method, this method should check whether this position is taken or no)
+
+def position_taken?(board, position)
+ if board[position] == " "
+ return false
+ elsif board[position] == ""
+ return false
+elsif board[position] == nil
+  return false
+elsif board[position] == "X" || "O"
+  return true
+end
+end
